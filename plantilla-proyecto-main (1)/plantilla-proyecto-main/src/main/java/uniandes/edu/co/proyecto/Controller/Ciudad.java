a15588047c8a0cdb8d2808366d145270718d0983
Carlos Vargas <cf.vargas@uniandes.edu.co>
2024-09-14 20:48:34 -0500

    Ciudad

--- plantilla-proyecto-main (1)/plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/Controller/Ciudad.java
+++ plantilla-proyecto-main (1)/plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/Controller/Ciudad.java
@@ -16,6 +16,28 @@ public class Ciudad {
     
     public Ciudad(){;}
     
+    public Ciudad(Integer id,String nombre) {
+        this.id = id;
+        this.nombre  = nombre ;
+        
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
     
     
