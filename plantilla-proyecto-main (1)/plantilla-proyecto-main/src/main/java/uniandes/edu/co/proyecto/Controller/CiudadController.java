e88ef31d2cb46021c49ce9cd7e59378db47771b0
Carlos Vargas <cf.vargas@uniandes.edu.co>
2024-09-14 21:34:38 -0500

    2do commit

--- plantilla-proyecto-main (1)/plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/Controller/CiudadController.java
+++ plantilla-proyecto-main (1)/plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/Controller/CiudadController.java
@@ -1,5 +1,51 @@
 package uniandes.edu.co.proyecto.Controller;
+import org.hibernate.mapping.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import uniandes.edu.co.proyecto.Model.Ciudad;
+import uniandes.edu.co.proyecto.Repository.CiudadRepository;
+
+import org.springframework.ui.Model;
+
+@Controller
 public class CiudadController {
+    @Autowired
+    private CiudadRepository ciudadRepository;
+    @GetMapping("/ciudad")
+public String ciudad(Model model, Integer id) {
+    if(id != null && !id.equals(null)) {
+        model.addAttribute("ciudad", ciudadRepository.darCiudadPorId(id));
+    } else {
+        model.addAttribute("ciudad", ciudadRepository.darCiudad());
+    }
+    return "ciudad";
+}
+
+
+
+
+
+    //  insertar nueva cuenta----------------------------------------------------------------------------------------
+    @GetMapping("/ciudad/new")
+    public String ciudadForm(Model model) {
+        model.addAttribute("ciudad", new Ciudad());
+        return "ciudadNuevo";
+    }
+
+    @PostMapping("/cuenta/new/save")
+    public String cuentaGuardar(@ModelAttribute Ciudad ciudad) {
+        ciudadRepository.insertarCiudad(ciudad.getNombre());
+        return "redirect:/ciudad";
+    }  
+//------------------------------------------------------------------------------------------------------   
+    
+
     
 }
