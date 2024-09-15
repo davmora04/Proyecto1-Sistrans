65af93e00eab28c05de617df7392be80b5ad88b0
Carlos Vargas <cf.vargas@uniandes.edu.co>
2024-09-14 22:50:42 -0500

    sucursal

---
+++ plantilla-proyecto-main (1)/plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/Model/Sucursal.java
@@ -0,0 +1,70 @@
+package uniandes.edu.co.proyecto.Model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Sucursal {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    private Integer id;
+    private String nombre;
+    private String ciudad;
+    private Integer tamano;
+    private String telefono;
+    public Sucursal(){;}
+    
+    public Sucursal(Integer id,String nombre, String ciudad,Integer tamano, String telefono ) {
+        this.id = id;
+        this.nombre  = nombre;
+        this.ciudad = ciudad;
+        this.tamano = tamano;
+        this.telefono = telefono;
+
+}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Integer getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(Integer tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
