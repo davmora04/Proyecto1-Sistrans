b9572201ed584cf11e60a61bbfa18f969cdec928
silvanaecheverry <s.echeverrym@uniandes.edu.co>
2024-09-21 10:08:03 -0500

    cambios

---
+++ plantilla-proyecto-main (1)/plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/Model/Categoria.java
@@ -0,0 +1,52 @@
+package uniandes.edu.co.proyecto.Model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_categoria;
+    private String nombre;
+    private String descripcion;
+    private String caracteristicasAlmacenamiento; 
+
+    public Categoria() {;}
+    public Categoria(Integer id_categoria, String nombre, String descripcion, String caracteristicasAlmacenamiento) {
+        this.id_categoria = id_categoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.caracteristicasAlmacenamiento = caracteristicasAlmacenamiento;
+    }
+    public Integer getId_categoria() {
+        return id_categoria;
+    }
+    public void setId_categoria(Integer id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getCaracteristicasAlmacenamiento() {
+        return caracteristicasAlmacenamiento;
+    }
+    public void setCaracteristicasAlmacenamiento(String caracteristicasAlmacenamiento) {
+        this.caracteristicasAlmacenamiento = caracteristicasAlmacenamiento;
+    }
+    
+
+    
+}
